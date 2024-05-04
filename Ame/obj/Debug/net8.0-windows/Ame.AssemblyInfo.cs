bc0e879be8d02532a0df184189e5b8c7ad0652c8
AdanRudieri <adanrudieri@gmail.com>
2024-05-04 19:53:08 -0300

    Criação dos formulários P7 e P8

--- Ame/obj/Debug/net8.0-windows/Ame.AssemblyInfo.cs
+++ Ame/obj/Debug/net8.0-windows/Ame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f36abed079da265d1e769e2997eb9b27c39284ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fecdac3e54d11c995a460377ef1e7e212bffae6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
