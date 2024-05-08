b6aeacd137a93039a00d90a43cd5ea606f869dc3
AdanRudieri <adanrudieri@gmail.com>
2024-05-08 10:57:39 -0300

    Criação do formulário P22, P23, P24 e correcao nos forms P11 e P16

--- Ame/obj/Debug/net8.0-windows/Ame.AssemblyInfo.cs
+++ Ame/obj/Debug/net8.0-windows/Ame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3833380f4b5ee199978061fcb15e8b6d08badb42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d644ffc5d3be1fe71690f7ee18ad163a159ffb59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
