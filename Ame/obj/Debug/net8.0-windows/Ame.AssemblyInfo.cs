70d8474915eabaeedb354d460163cad746b91b7d
AdanRudieri <adanrudieri@gmail.com>
2024-05-10 00:32:53 -0300

    Criação do formulário P27

--- Ame/obj/Debug/net8.0-windows/Ame.AssemblyInfo.cs
+++ Ame/obj/Debug/net8.0-windows/Ame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6aeacd137a93039a00d90a43cd5ea606f869dc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6a2842c92f09ae021467c74f753046d2bed6de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
