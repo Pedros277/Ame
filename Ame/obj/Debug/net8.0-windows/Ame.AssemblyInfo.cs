a06e9f8f02e4a65423cbbb08a662ed6f39a62683
AdanRudieri <adanrudieri@gmail.com>
2024-05-16 00:04:12 -0300

    Revisando formulário, checado até o frmP10 + criação do banco

--- Ame/obj/Debug/net8.0-windows/Ame.AssemblyInfo.cs
+++ Ame/obj/Debug/net8.0-windows/Ame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d37533bb9696511f7571e23cb63baa2e86cf90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a2cd7f9947864126ab0c67af74787aaf0f6237")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
